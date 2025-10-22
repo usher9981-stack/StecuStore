2b03dcd01aa2e8924440b57b1fa6dfa75185882e
usher9981-stack <usher9981@gmail.com>
2025-10-23 00:45:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function promo() {
+  alert("🎉 Promo Spesial! Diskon 30% untuk semua produk minggu ini!");
+}
+
+function tambahKeranjang(namaProduk) {
+  alert(`${namaProduk} telah ditambahkan ke keranjang 🛒`);
+}
